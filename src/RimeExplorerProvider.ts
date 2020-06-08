00e2ddb1aac37fe5ca1e4934dc141cb2aed93332
Mengqi Pei <mengqipei@gmail.com>
2020-06-08 23:04:04 +0800

    Change path to Windows style

--- src/RimeExplorerProvider.ts
+++ src/RimeExplorerProvider.ts
@@ -20,12 +20,12 @@ export class RimeExplorerProvider implements vscode.TreeDataProvider<TreeItem> {
             if (element.label === 'Default Configurations') {
                 // this.readRimeDefaultConfigurations();
                 // 'C:\Program Files (x86)\Rime\weasel-0.14.3'
-                let defaultConfigPath: string = '/' + path.join('mnt', 'C', 'Program Files (x86)', 'Rime', 'weasel-0.14.3');
+                let defaultConfigPath: string = path.join('C:', 'Program Files (x86)', 'Rime', 'weasel-0.14.3');
                 return this._getConfigFiles(defaultConfigPath);
             } else if (element.label === 'User Configurations') {
                 // this.readRimeUserConfigurations();
                 // C:\Users\mengq\AppData\Roaming\Rime
-                let userConfigPath: string = '/' + path.join('mnt', 'c', 'Users', 'mengq', 'AppData', 'Roaming', 'Rime');
+                let userConfigPath: string = path.join('C:', 'Users', 'mengq', 'AppData', 'Roaming', 'Rime');
                 return this._getConfigFiles(userConfigPath);
             }
         }
