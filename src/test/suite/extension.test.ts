874ffc9cbd4fe9e0f9cecb1136b0340be62cc4d8
Mengqi Pei <mengqipei@gmail.com>
2020-07-07 22:06:28 +0800

    Add test case for object tree having array

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -92,4 +92,34 @@ suite('Extension Test Suite', () => {
 			assert.fail(`Error occurred during assertion: ${error.message}`);
 		}
 	});
+
+	test('buildConfigTree_whenObjectTreeHasArray_expectNodeTreeBuilt', () => {
+		// Arrange.
+		const FILE_FULL_PATH: string = "C:/foo/bar/baz.yaml";
+		const FILE_NAME: string = "baz";
+		const IS_CUSTOM_CONFIG: boolean = false;
+		const rimeConfigurationTree: RimeConfigurationTreeForTest = new RimeConfigurationTreeForTest();
+		const rootNode: ConfigTreeItem = new ConfigTreeItem(FILE_NAME, [], FILE_FULL_PATH, 0, IS_CUSTOM_CONFIG);
+		const twoLayerObject: object = { a: '1', b: 2, c: [{c1: 31, c2: '32'}, {c3: 33}] };
+
+		const expectedChildNodeA: ConfigTreeItem = new ConfigTreeItem('a', [], FILE_FULL_PATH, 0, false, '1');
+		const expectedChildNodeB: ConfigTreeItem = new ConfigTreeItem('b', [], FILE_FULL_PATH, 0, false, 2);
+		const expectedChildNodeC1: ConfigTreeItem = new ConfigTreeItem('c1', [], FILE_FULL_PATH, 0, false, 31);
+		const expectedChildNodeC2: ConfigTreeItem = new ConfigTreeItem('c2', [], FILE_FULL_PATH, 0, false, '32');
+		const expectedChildNodeC3: ConfigTreeItem = new ConfigTreeItem('c3', [], FILE_FULL_PATH, 0, false, 33);
+		const expectedChildNodeC12: ConfigTreeItem = new ConfigTreeItem('c12', [expectedChildNodeC1, expectedChildNodeC2], FILE_FULL_PATH, 0, false);
+		const expectedChildNodeC: ConfigTreeItem = new ConfigTreeItem('c', [expectedChildNodeC12, expectedChildNodeC3], FILE_FULL_PATH, 0, false);
+		const expectedNodeBuilt: ConfigTreeItem = new ConfigTreeItem(FILE_NAME, [expectedChildNodeA, expectedChildNodeB, expectedChildNodeC] , FILE_FULL_PATH, 0, IS_CUSTOM_CONFIG);
+		expectedNodeBuilt.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+
+		// Act.
+		rimeConfigurationTree._buildConfigTree(twoLayerObject, rootNode, FILE_FULL_PATH, IS_CUSTOM_CONFIG);
+		
+		// Assert.
+		try {
+			assert.deepStrictEqual(rootNode, expectedNodeBuilt);
+		} catch (error) {
+			assert.fail(`Error occurred during assertion: ${error.message}`);
+		}
+	});
 });
