9236e124a48866d369258b938a07780d12fab4ee
Mengqi Pei <mengqipei@gmail.com>
2020-08-09 14:33:46 +0800

    Add language server example

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,63 @@
 'use strict';
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { RimeFileExplorerProvider } from './RimeFileExplorerProvider';
 import { RimeNodeExplorerProvider } from './RimeNodeExplorerProvider';
 import { RimeConfigurationTree, RimeConfigNode, FileKind } from './RimeConfigurationTree';
+import path = require('path');
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let client: LanguageClient;
+
 export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rime-assistant" is now active!');
+	//================================== RIME LANGUAGE CLIENT START ==========================================//
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+	run: { module: serverModule, transport: TransportKind.ipc },
+	debug: {
+		module: serverModule,
+		transport: TransportKind.ipc,
+		options: debugOptions
+	}
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+	// Register the server for plain text documents
+	documentSelector: [{ scheme: 'file', language: 'yaml' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	const client = new LanguageClient(
+		'rimeLanguagerServer',
+		'Rime Language Server',
+		serverOptions,
+		clientOptions
+	);
+	// Start the client. This will also launch the server
+	client.start();
+
+	//==================================== RIME LANGUAGE CLIENT END -=========================================//
+
+
+	//======================================= RIME EXPLORER START ============================================//
 
 	const rimeConfigurationTree: RimeConfigurationTree = new RimeConfigurationTree();
 	await rimeConfigurationTree.build();
@@ -39,7 +83,14 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('rimeNodeExplorer.refreshEntry', () => { rimeNodeExplorerProvider.refresh(); });
 	vscode.commands.registerCommand('rimeNodeExplorer.showOnlySchemaNodes', () => { rimeNodeExplorerProvider.showOnly(FileKind.Schema); });
 	vscode.commands.registerCommand('rimeNodeExplorer.showAll', () => { rimeNodeExplorerProvider.showOnly(undefined); });
+
+	//======================================= RIME EXPLORER END ============================================//
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
